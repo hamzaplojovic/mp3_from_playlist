4d2bd7539821cb225d960f3705f8f8f2e2b271d4
$ whoami <benorcunozdemir@gmail.com>
2018-06-14 13:41:27 +0300

    Add exception handling

--- youtube-video-mp3-converter.py
+++ youtube-video-mp3-converter.py
@@ -5,14 +5,18 @@ from pydub import AudioSegment
 import pytube
 import os
 
-video_url = input('Enter YouTube video URL: ')
+try:
+    video_url = input('Enter YouTube video URL: ')
 
-if os.name == 'nt':
-    path = os.getcwd() + '\\'
-else:
-    path = os.getcwd() + '/'
+    if os.name == 'nt':
+        path = os.getcwd() + '\\'
+    else:
+        path = os.getcwd() + '/'
 
-name = pytube.extract.video_id(video_url)
-YouTube(video_url).streams.filter(only_audio=True).first().download(filename=name)
-location = path + name + '.mp4'
-AudioSegment.from_file(location).export(name + '.mp3', format="mp3")
+        name = pytube.extract.video_id(video_url)
+        YouTube(video_url).streams.filter(only_audio=True).first().download(filename=name)
+        location = path + name + '.mp4'
+        AudioSegment.from_file(location).export(name + '.mp3', format="mp3")
+        print('Finished successfully!')
+except:
+    print('Something went wrong!')
